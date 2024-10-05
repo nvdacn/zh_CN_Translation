d108c263e388096581035bb5cff7e54f3414d963
WMHN <1872265132@qq.com>
2024-10-05 16:36:59 +0800

    添加 nvdaL10nUtil 相关的批处理脚本及必要文件 (#7)
    不包含 nvdaL10nUtil 程序，如需使用此存储库提供的批处理脚本，需要从 NV Access 的相关网站下载 nvdaL10nUtil.exe，并将其复制到此存储库的 `./Tools` 文件夹。

---
+++ 为已翻译文档生成html文件.bat
@@ -0,0 +1,11 @@
+@echo off
+
+for /r "%~dp0Preview" %%i in (*.html) do (
+  del /f /q "%%i"
+)
+
+"%~dp0Tools\nvdaL10nUtil.exe" xliff2html -t changes "%~dp0Translation\user_docs\changes.xliff" "%~dp0Preview\changes.html"
+"%~dp0Tools\nvdaL10nUtil.exe" xliff2html -t keyCommands "%~dp0Translation\user_docs\userGuide.xliff" "%~dp0Preview\keyCommands.html"
+"%~dp0Tools\nvdaL10nUtil.exe" xliff2html -t userGuide "%~dp0Translation\user_docs\userGuide.xliff" "%~dp0Preview\userGuide.html"
+
+Exit
