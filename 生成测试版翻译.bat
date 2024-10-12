cf96efd3d2e2b37565589348255ae5f65a91e2a5
WMHN1872265132 <1872265132@qq.com>
2024-10-12 14:22:45 +0800

    调整生成测试版翻译.bat
    不在通过调用为已翻译文档生成html文件.bat生成文档翻译

--- 生成测试版翻译.bat
+++ 生成测试版翻译.bat
@@ -5,7 +5,6 @@ chcp 65001
 for /f "delims=" %%o in ('git branch --show-current') do set Branch=%%o
 
 if "%1" == "GITHUB_ACTIONS" (
-  @echo on
   set DateTime=%date:~-10,2%%date:~-7,2%%time:~0,2%%time:~3,2%
 ) else (
   set DateTime=%date:~8,2%%date:~11,2%%time:~0,2%%time:~3,2%
@@ -23,13 +22,12 @@ IF EXIST "%~dp0Preview\Archive" (rd /s /q "%~dp0Preview\Archive")
 MKDir "%~dp0Preview\Output\locale\zh_CN\LC_MESSAGES"
 "%~dp0Tools\msgfmt.exe" -o "%~dp0Preview\Output\locale\zh_CN\LC_MESSAGES\nvda.mo" "%~dp0Translation\LC_MESSAGES\nvda.po"
 
-Start /Wait /D "%~dp0" 为已翻译文档生成html文件.bat
 MKDir "%~dp0Preview\Output\documentation\zh_CN"
 MKLINK /H "%~dp0Preview\Output\documentation\zh_CN\numberedHeadings.css" "%~dp0Preview\numberedHeadings.css"
 MKLINK /H "%~dp0Preview\Output\documentation\zh_CN\styles.css" "%~dp0Preview\styles.css"
-MKLINK /H "%~dp0Preview\Output\documentation\zh_CN\changes.html" "%~dp0Preview\changes.html"
-MKLINK /H "%~dp0Preview\Output\documentation\zh_CN\keyCommands.html" "%~dp0Preview\keyCommands.html"
-MKLINK /H "%~dp0Preview\Output\documentation\zh_CN\userGuide.html" "%~dp0Preview\userGuide.html"
+"%~dp0Tools\nvdaL10nUtil.exe" xliff2html -t changes "%~dp0Translation\user_docs\changes.xliff" "%~dp0Preview\Output\documentation\zh_CN\changes.html"
+"%~dp0Tools\nvdaL10nUtil.exe" xliff2html -t keyCommands "%~dp0Translation\user_docs\userGuide.xliff" "%~dp0Preview\Output\documentation\zh_CN\keyCommands.html"
+"%~dp0Tools\nvdaL10nUtil.exe" xliff2html -t userGuide "%~dp0Translation\user_docs\userGuide.xliff" "%~dp0Preview\Output\documentation\zh_CN\userGuide.html"
 
 "%~dp0Tools\7Zip\7z.exe" a -sccUTF-8 -y -tzip "%~dp0Preview\Archive\NVDA_%Branch%_翻译测试（解压到NVDA程序文件夹）_%VersionInfo%.zip" "%~dp0Preview\Output\documentation" "%~dp0Preview\Output\locale"
 
