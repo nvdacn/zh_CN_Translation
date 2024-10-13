93f976875ef9d641179a54886fc26560f3f07e4b
WMHN1872265132 <1872265132@qq.com>
2024-10-13 14:11:00 +0800

    代码格式微调

--- 文件生成工具.bat
+++ 文件生成工具.bat
@@ -3,18 +3,18 @@
 chcp 65001
 
 if "%1" == "GITHUB_ACTIONS" (
-set CLI=T
-goto T
+  set CLI=T
+  goto T
 )
 
 IF not EXIST "%~dp0Tools\nvdaL10nUtil.exe" (
   mshta "javascript:new ActiveXObject('wscript.shell').popup('文件 nvdaL10nUtil.exe 不存在，请下载该程序并将其复制到 Tools 文件夹后重试。',5,'文件不存在');window.close();"
-Exit
+  Exit
 )
 
 if not "%1"=="" (
-set CLI=%1
-goto goto
+  set CLI=%1
+  goto goto
 )
 
 cls
