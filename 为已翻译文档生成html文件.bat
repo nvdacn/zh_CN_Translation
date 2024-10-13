99ca46c7e0033e0b311d5f1b8bcb413ee0265646
WMHN1872265132 <1872265132@qq.com>
2024-10-13 09:25:10 +0800

    合并生成可上传的changes/userGuide文档批处理至为已翻译文档生成html文件.bat

--- 为已翻译文档生成html文件.bat
+++ 为已翻译文档生成html文件.bat
@@ -15,19 +15,18 @@ echo 请选择要生成的文件，按回车键执行。
 echo C：生成更新日志的html文件；
 echo u：生成用户指南的html文件；
 echo K：生成热键快速参考的html文件；
-echo A：生成所有文档的html文件；
+echo DOC：生成所有文档的html文件；
+echo STC：生成可直接上传到Crowdin的changes文档，需要将原始xliff文件放入存储库的Crowdin\OldXLIFF文件夹下，如未检测到该文件，系统会从存储库的默认分支提取；
+echo STU：生成可直接上传到Crowdin的userGuide文档，需要将原始xliff文件放入存储库的Crowdin\OldXLIFF文件夹下，如未检测到该文件，系统会从存储库的默认分支提取；
 echo 其他命令：退出本工具。
 echo 上述选项还可通过命令行直接传入。
 
 set /p CLI=
 
 :goto
-for /r "%~dp0Preview" %%i in (*.html) do (
-  del /f /q "%%i"
-)
 goto %CLI%
 
-:A
+:DOC
 :C
 "%~dp0Tools\nvdaL10nUtil.exe" xliff2html -t changes "%~dp0Translation\user_docs\changes.xliff" "%~dp0Preview\changes.html"
 if /I "%CLI%"=="C" (Exit)
@@ -38,5 +37,38 @@ if /I "%CLI%"=="U" (Exit)
 
 :K
 "%~dp0Tools\nvdaL10nUtil.exe" xliff2html -t keyCommands "%~dp0Translation\user_docs\userGuide.xliff" "%~dp0Preview\keyCommands.html"
+if /I "%CLI%"=="K" (Exit)
+if /I "%CLI%"=="DOC" (Exit)
+
+:STC
+:STU
+if /I "%CLI%"=="STC" (
+set ST=changes
+goto ST
+) 
+if /I "%CLI%"=="STU" (
+set ST=userGuide
+goto ST
+)
+exit
+
+:ST
+IF EXIST "%~dp0Crowdin\OldXLIFF\Temp" (
+rd /s /q "%~dp0Crowdin\OldXLIFF\Temp"
+)
+
+MKDir "%~dp0Crowdin\OldXLIFF\Temp"
+
+IF EXIST "%~dp0%ST%.xliff" (del /f /q "%~dp0%ST%.xliff")
+
+IF Not EXIST "%~dp0Crowdin\OldXLIFF\%ST%.xliff" (
+git archive --output "./Crowdin/OldXLIFF/Temp/%ST%.zip" 2025.1 Translation/user_docs/%ST%.xliff
+"%~dp0Tools\7Zip\7z.exe" e "%~dp0Crowdin\OldXLIFF\Temp\%ST%.zip" "Translation\user_docs\%ST%.xliff" -aoa -o"%~dp0Crowdin\OldXLIFF"
+)
+
+  MKLINK /H "%~dp0Crowdin\OldXLIFF\Temp\%ST%_Old.xliff" "%~dp0Crowdin\OldXLIFF\%ST%.xliff"
+MKLINK /H "%~dp0Crowdin\OldXLIFF\Temp\%ST%_Translated.xliff" "%~dp0Translation\user_docs\%ST%.xliff"
+
+"%~dp0Tools\nvdaL10nUtil.exe" stripXliff -o "%~dp0Crowdin\OldXLIFF\Temp\%ST%_Old.xliff" "%~dp0Crowdin\OldXLIFF\Temp\%ST%_Translated.xliff" "%~dp0Crowdin\%ST%.xliff"
 
 Exit
