d4ed27326f98d95af0c415aa11a7a7692c130e8e
WMHN1872265132 <1872265132@qq.com>
2024-10-13 13:43:27 +0800

    支持GitHub工作流的调用

--- 文件生成工具.bat
+++ 文件生成工具.bat
@@ -2,6 +2,11 @@
 
 chcp 65001
 
+if "%1" == "GITHUB_ACTIONS" (
+set CLI=T
+goto T
+)
+
 IF not EXIST "%~dp0Tools\nvdaL10nUtil.exe" (
   mshta "javascript:new ActiveXObject('wscript.shell').popup('文件 nvdaL10nUtil.exe 不存在，请下载该程序并将其复制到 Tools 文件夹后重试。',5,'文件不存在');window.close();"
 Exit
