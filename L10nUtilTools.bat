a7f01d60cd5bd90304d11d83d2a2e57c74667592
WMHN1872265132 <1872265132@qq.com>
2025-07-30 18:12:48 +0800

    L10nUtilTools.bat：重构生成翻译预览系列命令

--- L10nUtilTools.bat
+++ L10nUtilTools.bat
@@ -5,7 +5,6 @@ Title L10n Util Tools
 
 Rem 为避免出现编码错误，请在行末是中文字符的行尾添加两个空格  
 Rem 设置 nvdaL10nUtil 程序路径  
-set ExitCode=0
 for %%F in (
   "%ProgramFiles%\NVDA\l10nUtil.exe"
   "%ProgramFiles(x86)%\NVDA\l10nUtil.exe"
@@ -48,7 +47,7 @@ for /f "tokens=1,2 delims=_" %%A in ("%ProcessCLI%") do (
   set "CLIPart1=%%A"
   set "CLIPart2=%%B"
 )
-if /I "%CLIPart1%"=="BD" (set "CLIPart1=")
+if /I "%CLIPart1%"=="BD" (set "CLIPart1=GE")
 set "replacements=TEST:T nvda:L changes:C userGuide:U"
 for %%R in (%replacements%) do (
   for /f "tokens=1,2 delims=:" %%A in ("%%R") do (
@@ -63,13 +62,12 @@ Rem 打印可用命令
 :Echo
 cls
 echo 欢迎使用 L10nUtilTools，请输入要执行的操作，按回车键确认。  
-echo C：生成更新日志的 html 文件；  
-echo U：生成用户指南的 html 文件；  
-echo K：生成热键快速参考的 html 文件；  
-echo D：生成所有文档的 html 文件；  
-echo L：生成界面翻译的 mo 文件；  
-echo T：生成翻译测试文件（不压缩）；  
-echo Z：生成翻译测试文件的压缩包；  
+echo GEC：生成更新日志的 html 文件；  
+echo GEU：生成用户指南的 html 文件；  
+echo GEK：生成热键快速参考的 html 文件；  
+echo GEL：生成界面翻译的 mo 文件；  
+echo GET：生成翻译测试文件（不压缩）；  
+echo GEZ：生成翻译测试文件的压缩包；  
 echo UDL：从给定的 nvda.pot 更新 nvda.po 的翻译字符串；  
 echo UPC：上传已翻译的 changes.xliff 文件到 Crowdin；  
 echo UPU：上传已翻译的 userGuide.xliff 文件到 Crowdin；  
@@ -90,91 +88,13 @@ echo 上述选项还可通过命令行直接传入。
 Rem 等待用户输入  
 set /p CLI=
 
-Rem 跳转到用户输入的命令或退出  
+Rem 初始化变量，跳转到用户输入的命令或退出  
 :goto
+set ExitCode=0
+set Parameter=%CLI:~0,2%
 cls
 goto %CLI% >Nul
 exit
-
-Rem 生成文档的流程，此部分命令会连续执行，直到符合输入的命令后退出  
-Rem 生成更新日志  
-:C
-:D
-:T
-:Z
-IF EXIST "%~dp0Preview\changes.html" (del /f /q "%~dp0Preview\changes.html")
-%L10nUtil% xliff2html -t changes "%~dp0Translation\user_docs\changes.xliff" "%~dp0Preview\changes.html"
-if /I "%CLI%"=="C" (
-  set ExitCode=%errorlevel%
-  goto Quit
-)
-
-Rem 生成用户指南  
-:U
-IF EXIST "%~dp0Preview\userGuide.html" (del /f /q "%~dp0Preview\userGuide.html")
-%L10nUtil% xliff2html -t userGuide "%~dp0Translation\user_docs\userGuide.xliff" "%~dp0Preview\userGuide.html"
-if /I "%CLI%"=="U" (
-  set ExitCode=%errorlevel%
-  goto Quit
-)
-
-Rem 生成热键快速参考  
-:K
-IF EXIST "%~dp0Preview\keyCommands.html" (del /f /q "%~dp0Preview\keyCommands.html")
-%L10nUtil% xliff2html -t keyCommands "%~dp0Translation\user_docs\userGuide.xliff" "%~dp0Preview\keyCommands.html"
-if /I "%CLI%"=="K" (
-  set ExitCode=%errorlevel%
-  goto Quit
-)
-if /I "%CLI%"=="D" (
-  set ExitCode=%errorlevel%
-  goto Quit
-)
-
-Rem 生成界面翻译  
-:L
-IF EXIST "%~dp0Preview\nvda.mo" (del /f /q "%~dp0Preview\nvda.mo")
-"%~dp0Tools\msgfmt.exe" -o "%~dp0Preview\nvda.mo" "%~dp0Translation\LC_MESSAGES\nvda.po"
-if /I "%CLI%"=="L" (
-  set ExitCode=%errorlevel%
-  goto Quit
-)
-
-Rem 生成NVDA翻译目录结构  
-IF EXIST "%~dp0Preview\Test" (rd /s /q "%~dp0Preview\Test")
-MKDir "%~dp0Preview\Test\locale\zh_CN\LC_MESSAGES"
-MKLINK /H "%~dp0Preview\Test\locale\zh_CN\LC_MESSAGES\nvda.mo" "%~dp0Preview\nvda.mo"
-MKLINK /H "%~dp0Preview\Test\locale\zh_CN\characterDescriptions.dic" "%~dp0Translation\miscDeps\characterDescriptions.dic"
-MKLINK /H "%~dp0Preview\Test\locale\zh_CN\gestures.ini" "%~dp0Translation\miscDeps\gestures.ini"
-MKLINK /H "%~dp0Preview\Test\locale\zh_CN\symbols.dic" "%~dp0Translation\miscDeps\symbols.dic"
-MKDir "%~dp0Preview\Test\documentation\zh_CN"
-MKLINK /H "%~dp0Preview\Test\documentation\zh_CN\favicon.ico" "%~dp0Preview\favicon.ico"
-MKLINK /H "%~dp0Preview\Test\documentation\zh_CN\numberedHeadings.css" "%~dp0Preview\numberedHeadings.css"
-MKLINK /H "%~dp0Preview\Test\documentation\zh_CN\styles.css" "%~dp0Preview\styles.css"
-MKLINK /H "%~dp0Preview\Test\documentation\zh_CN\changes.html" "%~dp0Preview\changes.html"
-MKLINK /H "%~dp0Preview\Test\documentation\zh_CN\keyCommands.html" "%~dp0Preview\keyCommands.html"
-MKLINK /H "%~dp0Preview\Test\documentation\zh_CN\userGuide.html" "%~dp0Preview\userGuide.html"
-if /I "%CLI%"=="T" (
-  set ExitCode=%errorlevel%
-  goto Quit
-)
-
-Rem 获取当前分支名称、系统的日期和时间作为翻译测试压缩包的部分名称  
-for /f "delims=" %%o in ('git branch --show-current') do set Branch=%%o
-set DateTime=%date:~8,2%%date:~11,2%%time:~0,2%%time:~3,2%
-If "%DateTime:~4,1%" == " " (
-  set VersionInfo=%DateTime:~0,4%0%DateTime:~5,3%
-) Else (
-  set VersionInfo=%DateTime%
-)
-
-Rem 生成翻译测试压缩包  
-IF EXIST "%~dp0Preview\Archive" (rd /s /q "%~dp0Preview\Archive")
-"%~dp0Tools\7Zip\7z.exe" a -sccUTF-8 -y -tzip "%~dp0Preview\Archive\NVDA_%Branch%_翻译测试（解压到NVDA程序文件夹）_%VersionInfo%.zip" "%~dp0Preview\Test\documentation" "%~dp0Preview\Test\locale"
-if /I "%CLI%"=="Z" (
-  set ExitCode=%errorlevel%
-  goto Quit
-)
 
 Rem 从给定的 nvda.pot 更新界面翻译字符串  
 :UDL
@@ -203,7 +123,13 @@ msgmerge.exe --update --backup=none --previous "%~dp0Translation\LC_MESSAGES\nvd
 set ExitCode=%errorlevel%
 goto Quit
 
-Rem 处理标签  
+Rem 处理标签，初始化变量  
+:GEC
+:GEU
+:GEK
+:GEL
+:GET
+:GEZ
 :DLL
 :DLC
 :DLU
@@ -216,18 +142,25 @@ Rem 处理标签
 :UPC
 :UPU
 :UPA
+if /I  %CLI:~0,2%==GE (set Action=GenerateFiles)
 if /I  %CLI:~0,2%==DL (set Action=DownloadFiles)
 if /I  %CLI:~0,2%==DC (
   cd /d "%~dp0"
   set Action=DownloadAndCommit
 )
 if /I  %CLI:~0,2%==UP (set Action=UploadFiles)
+if /I %CLI:~2,1%==T (
+  set Type=Test
+  goto TranslationTest
+)
+if /I %CLI:~2,1%==Z (
+  set Type=Archive
+  goto TranslationTest
+)
 if /I %CLI:~2,1%==A (
   set Type=All
   if /I %Action%==DownloadAndCommit (
     set Parameter=DL
-  ) else (
-    set Parameter=%CLI:~0,2%
   )
   goto All
 )
@@ -236,14 +169,22 @@ if /I %CLI:~2,1%==L (
   set GitAddPath=Translation/LC_MESSAGES
   set TranslationPath=%~dp0Translation\LC_MESSAGES
   set FileName=nvda.po
+  set ShortName=nvda
 )
 if /I %CLI:~2,1%==C (
   set Type=Docs
   set FileName=changes.xliff
+  set ShortName=changes
 )
 if /I %CLI:~2,1%==U (
   set Type=Docs
   set FileName=userGuide.xliff
+  set ShortName=userGuide
+)
+if /I %CLI:~2,1%==K (
+  set Type=Docs
+  set FileName=userGuide.xliff
+  set ShortName=keyCommands
 )
 if /I %Type%==Docs (
   set GitAddPath=Translation/user_docs
@@ -251,7 +192,67 @@ if /I %Type%==Docs (
 )
 goto %Action%
 
-Rem **A 系列命令：通过循环调用另一个L10nUtilTools.bat来分别处理  
+Rem 生成翻译预览系列命令  
+:GenerateFiles
+if /I "%Type%" == "LC_MESSAGES" (
+  IF EXIST "%~dp0Preview\%ShortName%.mo" (del /f /q "%~dp0Preview\%ShortName%.mo")
+  IF EXIST "%TranslationPath%\%ShortName%.mo" (del /f /q "%TranslationPath%\%ShortName%.mo")
+  "%~dp0Tools\msgfmt.exe" -o "%~dp0Preview\%ShortName%.mo" "%TranslationPath%\%FileName%"
+  set ExitCode=!errorlevel!
+  MKLINK /H "%TranslationPath%\%ShortName%.mo" "%~dp0Preview\%ShortName%.mo"
+)
+if /I "%Type%" == "Docs" (
+  IF EXIST "%~dp0Preview\%ShortName%.html" (del /f /q "%~dp0Preview\%ShortName%.html")
+  %L10nUtil% xliff2html -t %ShortName% "%TranslationPath%\%FileName%" "%~dp0Preview\%ShortName%.html"
+  set ExitCode=!errorlevel!
+)
+goto Quit
+
+Rem GET 和 GEZ 命令的文件生成阶段：通过循环调用另一个 L10nUtilTools.bat 来分别处理  
+:TranslationTest
+for %%i in (L C U K) do (
+  cmd /C "%~dp0L10nUtilTools" %Parameter%%%i
+  if !errorlevel! neq 0 (
+    echo Error: Command %Parameter%%%i failed with exit code !errorlevel!.
+    exit /b !errorlevel!
+  )
+)
+
+Rem 生成NVDA翻译目录结构  
+IF EXIST "%~dp0Preview\Test" (rd /s /q "%~dp0Preview\Test")
+MKDir "%~dp0Preview\Test\locale\zh_CN\LC_MESSAGES"
+MKLINK /H "%~dp0Preview\Test\locale\zh_CN\LC_MESSAGES\nvda.mo" "%~dp0Preview\nvda.mo"
+MKLINK /H "%~dp0Preview\Test\locale\zh_CN\characterDescriptions.dic" "%~dp0Translation\miscDeps\characterDescriptions.dic"
+MKLINK /H "%~dp0Preview\Test\locale\zh_CN\gestures.ini" "%~dp0Translation\miscDeps\gestures.ini"
+MKLINK /H "%~dp0Preview\Test\locale\zh_CN\symbols.dic" "%~dp0Translation\miscDeps\symbols.dic"
+MKDir "%~dp0Preview\Test\documentation\zh_CN"
+MKLINK /H "%~dp0Preview\Test\documentation\zh_CN\favicon.ico" "%~dp0Preview\favicon.ico"
+MKLINK /H "%~dp0Preview\Test\documentation\zh_CN\numberedHeadings.css" "%~dp0Preview\numberedHeadings.css"
+MKLINK /H "%~dp0Preview\Test\documentation\zh_CN\styles.css" "%~dp0Preview\styles.css"
+MKLINK /H "%~dp0Preview\Test\documentation\zh_CN\changes.html" "%~dp0Preview\changes.html"
+MKLINK /H "%~dp0Preview\Test\documentation\zh_CN\keyCommands.html" "%~dp0Preview\keyCommands.html"
+MKLINK /H "%~dp0Preview\Test\documentation\zh_CN\userGuide.html" "%~dp0Preview\userGuide.html"
+if /I "%Type%" == "Test" (
+  set ExitCode=%errorlevel%
+  goto Quit
+)
+
+Rem 获取当前分支名称、系统的日期和时间作为翻译测试压缩包的部分名称  
+for /f "delims=" %%o in ('git branch --show-current') do set Branch=%%o
+set DateTime=%date:~8,2%%date:~11,2%%time:~0,2%%time:~3,2%
+If "%DateTime:~4,1%" == " " (
+  set VersionInfo=%DateTime:~0,4%0%DateTime:~5,3%
+) Else (
+  set VersionInfo=%DateTime%
+)
+
+Rem 生成翻译测试压缩包  
+IF EXIST "%~dp0Preview\Archive" (rd /s /q "%~dp0Preview\Archive")
+"%~dp0Tools\7Zip\7z.exe" a -sccUTF-8 -y -tzip "%~dp0Preview\Archive\NVDA_%Branch%_翻译测试（解压到NVDA程序文件夹）_%VersionInfo%.zip" "%~dp0Preview\Test\documentation" "%~dp0Preview\Test\locale"
+set ExitCode=%errorlevel%
+goto Quit
+
+Rem A 系列命令：通过循环调用另一个L10nUtilTools.bat来分别处理  
 :All
 for %%i in (L C U) do (
   cmd /C "%~dp0L10nUtilTools" %Parameter%%%i
