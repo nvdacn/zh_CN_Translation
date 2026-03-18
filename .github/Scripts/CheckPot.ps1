76fdb345cefef6e643986928e393b753e42a717d
WMHN1872265132 <1872265132@qq.com>
2026-03-18 10:40:17 +0800

    变量改进

--- .github/Scripts/CheckPot.ps1
+++ .github/Scripts/CheckPot.ps1
@@ -1,18 +1,18 @@
 $ErrorActionPreference = "Stop"
 git config --global user.name "GitHub Actions"
 git config --global user.email "actions@github.com"
-&7z.exe e "${{ github.workspace }}\PotXliff\Temp\gettext.zip" "bin\msgmerge.exe" -aoa -o"${{ github.workspace }}\Tools"
-cd "${{ github.workspace }}/Tools/NVDA"
+&7z.exe e "$env:GITHUB_WORKSPACE\PotXliff\Temp\gettext.zip" "bin\msgmerge.exe" -aoa -o"$env:GITHUB_WORKSPACE\Tools"
+cd "$env:GITHUB_WORKSPACE\Tools\NVDA"
 $commit = (git rev-parse HEAD).Substring(0,8)
 &runcheckpot.bat --all-cores
-cd "${{ github.workspace }}"
-&"${{ github.workspace }}\Tools\msgmerge.exe" --update  --backup=none --previous "${{ github.workspace }}\Translation\LC_MESSAGES\nvda.po" "${{ github.workspace }}\Tools\NVDA\output\nvda.pot"
+cd "$env:GITHUB_WORKSPACE"
+&"$env:GITHUB_WORKSPACE\Tools\msgmerge.exe" --update  --backup=none --previous "$env:GITHUB_WORKSPACE\Translation\LC_MESSAGES\nvda.po" "$env:GITHUB_WORKSPACE\Tools\NVDA\output\nvda.pot"
 git add "Translation/LC_MESSAGES/nvda.po"
 git diff --cached --quiet
 if ($LASTEXITCODE -ne 0) {
     git commit -m "更新 NVDA 界面消息翻译字符串（alpha-$commit）"
     git pull --rebase
-    git push origin ${{ needs.Determine-Branch.outputs.branch }}:${{ needs.Determine-Branch.outputs.branch }}
+    git push origin $env:branch:$env:branch
 } else {
     Write-Host "No changes to commit, skipping commit and push."
 }
