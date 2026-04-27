f74ce7e7dbff60f91d9a875a131d316470a09769
WMHN1872265132 <1872265132@qq.com>
2026-04-27 10:21:12 +0800

    还原 "L10nUtilTools.bat：删除 UAX 和 DAX 命令"
    This reverts commit 10830e891288b3089a2a6159445879ab86954784.

--- L10nUtilTools.bat
+++ L10nUtilTools.bat
@@ -128,8 +128,10 @@ Pause>Nul
 echo GMX：使用指定插件的 readme.xliff 生成 Markdown 文件；  
 echo MXX：使用指定插件的 readme.md 文档生成可上传的 XLIFF 文件；  
 echo UAP：上传指定插件的界面翻译到 Crowdin；  
+echo UAX：上传指定插件的 xliff 文档翻译到 Crowdin；  
 echo UAM：上传指定插件的文档翻译到 Crowdin；  
 echo DAP：从 Crowdin 下载指定插件的界面翻译；  
+echo DAX：从 Crowdin 下载指定插件的 xliff 文档翻译；  
 echo DAM：从 Crowdin 下载指定插件的文档翻译；  
 echo CLE：清理上述命令生成的所有文件；  
 echo 其他命令：退出本工具。  
@@ -481,8 +483,10 @@ Rem 处理针对插件翻译的标签，初始化变量及运行环境
 :GMX
 :MXX
 :UAP
+:UAX
 :UAM
 :DAP
+:DAX
 :DAM
 Rem 此段代码将在 NVDA 使用 nvdaL10n 提供的 L10nUtil 时删除  
 set "goto=setConfigFilename"
@@ -520,11 +524,6 @@ if /I "%CLI:~2,1%"=="M" (
   set CrowdinFilePath=%AddonName%.md
   set FileName=readme.md
   set ShortName=%AddonName%
-  findstr /i "%AddonName%.xliff" "!ConfigFilename!" >nul 2>nul
-  if not !errorlevel! EQU 1 (
-    set CrowdinFilePath=%AddonName%.xliff
-    set FileName=readme.xliff
-  )
 )
 set TranslationPath=%~dp0Translation\Addons\%AddonName%
 IF NOT EXIST "%TranslationPath%" (MKDir "%TranslationPath%")
