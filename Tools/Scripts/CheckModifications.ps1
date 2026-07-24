ac0d0979fc19c6bab6b6a477f9de7a393fbc9dc3
WMHN1872265132 <1872265132@qq.com>
2026-07-24 12:01:46 +0800

    添加对 xliff 文件的支持

--- Tools/Scripts/CheckModifications.ps1
+++ Tools/Scripts/CheckModifications.ps1
@@ -1,8 +1,31 @@
-# 检查 nvda.po 文件的差异，如果只修改了 `POT-Creation-Date` 和 `PO-Revision-Date` 行则还原  
 param(
-    [Parameter(Mandatory=$true, HelpMessage="The path to the po file to be checked")]
+    [Parameter(Mandatory=$true, HelpMessage="The path to the file to be checked")]
     [string]$file
 )
+
+# 初始化变量  
+$shouldRevert = $true
+$checkPatterns = $null
+$fileExtension = [System.IO.Path]::GetExtension($file).ToLower()
+
+# 根据文件类型设置要检查的字符串模式
+switch ($fileExtension) {
+    '.po' {
+        $checkPatterns = @(
+            '^[-+]"POT-Creation-Date:',
+            '^[-+]"PO-Revision-Date:'
+        )
+    }
+    '.xliff' {
+        $checkPatterns = @(
+            '^[-+]  <file id'
+        )
+    }
+    default {
+        Write-Host "Unsupported file type: $fileExtension. Exiting..."
+        exit 0
+    }
+}
 
 # 获取文件的 git diff 输出  
 $diffOutput = git diff --unified=0 $file
@@ -13,24 +36,26 @@ if (-not $diffOutput) {
     exit 0
 }
 
-# 初始化变量  
-$onlyDateChanges = $true
-$datePattern1 = '^[-+]"POT-Creation-Date:'
-$datePattern2 = '^[-+]"PO-Revision-Date:'
-
 # 逐行分析 diff 输出  
 foreach ($line in $diffOutput -split "`n") {
     if (($line.StartsWith("+") -or $line.StartsWith("-")) -and (-not $line.StartsWith("+++")) -and (-not $line.StartsWith("---"))) {
-        if (-not ($line -match $datePattern1) -and -not ($line -match $datePattern2)) {
-            $onlyDateChanges = $false
+        $isAllowedLine = $false
+        foreach ($pattern in $checkPatterns) {
+            if ($line -match $pattern) {
+                $isAllowedLine = $true
+                break
+            }
+        }
+        if (-not $isAllowedLine) {
+            $shouldRevert = $false
             break
         }
     }
 }
 
 # 根据检查结果执行操作  
-if ($onlyDateChanges) {
-    Write-Host "Detected only date line modifications. Reverting file..."
+if ($shouldRevert) {
+    Write-Host "No meaningful modifications detected. Reverting the file..."
     Git restore $file
 } else {
     Write-Host "Substantial changes detected. Keeping modifications."
