6cab5080e6dc49e3e2d68c54dfcf683562e6cd72
WMHN <1872265132@qq.com>
2026-07-24 14:39:11 +0800

    忽略插件的 xliff 文件只有 file id 发生变化的更改 (#108)

--- L10nUtilTools.bat
+++ L10nUtilTools.bat
@@ -450,7 +450,10 @@ if %errorlevel% neq 0 (
   goto Quit
 )
 if /I "%Type%"=="LC_MESSAGES" (
-powershell -ExecutionPolicy Bypass -File "%~dp0Tools\Scripts\CheckPo.ps1" "%DownloadFilename%"
+powershell -ExecutionPolicy Bypass -File "%~dp0Tools\Scripts\CheckModifications.ps1" "%DownloadFilename%"
+)
+if /I "%FileName%"=="readme.xliff" (
+powershell -ExecutionPolicy Bypass -File "%~dp0Tools\Scripts\CheckModifications.ps1" "%DownloadFilename%"
 )
 if /I "%Action%"=="DownloadAndCommit" (goto Commit)
 exit /b 0
